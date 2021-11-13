fc0b6d53182509101ff5e5d9f007e43c94bfde6e
khadetou <khadetou96@gmail.com>
2021-11-13 17:45:13 +0000

    profile page and class

--- src/main/java/beans/profile/Profile.java
+++ src/main/java/beans/profile/Profile.java
@@ -1,5 +1,115 @@
 package beans.profile;
 
-public class Profile {
+import javax.faces.bean.ManagedBean;
 
+@ManagedBean
+public class Profile {
+	private String status;
+	private String website;
+	private String location;
+	private String company;
+	private String skills;
+	private String githubusername;
+	private String bio;
+	private String twitter;
+	private String facebook;
+	private String youtube;
+	private String linkedin;
+	private String instagram;
+	
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getSkills() {
+		return skills;
+	}
+
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+
+	public String getGithubusername() {
+		return githubusername;
+	}
+
+	public void setGithubusername(String githubUsername) {
+		this.githubusername = githubUsername;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+	public String getTwitter() {
+		return twitter;
+	}
+
+	public void setTwitter(String twitter) {
+		this.twitter = twitter;
+	}
+
+	public String getFacebook() {
+		return facebook;
+	}
+
+	public void setFacebook(String facebook) {
+		this.facebook = facebook;
+	}
+
+	public String getYoutube() {
+		return youtube;
+	}
+
+	public void setYoutube(String youtube) {
+		this.youtube = youtube;
+	}
+
+	public String getLinkedin() {
+		return linkedin;
+	}
+
+	public void setLinkedin(String linkedin) {
+		this.linkedin = linkedin;
+	}
+
+	public String getInstagram() {
+		return instagram;
+	}
+
+	public void setInstagram(String instagram) {
+		this.instagram = instagram;
+	}
 }
