2a7bc5551108cdaf2b42dd4636ac7f6d95d380c6
khadetou <khadetou96@gmail.com>
2021-11-14 17:22:27 +0000

    socials class

--- src/main/java/beans/profile/Socials.java
+++ src/main/java/beans/profile/Socials.java
@@ -1,5 +1,8 @@
 package beans.profile;
 
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
 public class Socials {
 	private int id;
 	private int profile_id;
@@ -13,10 +16,54 @@ public class Socials {
 		
 	}
 	public Socials(int id, int profile_id, String youtube, String twitter, String facebook, String linkedin, String instagram) {
+		this.setId(id);
+		this.setProfile_id(profile_id);
+		this.setYoutube(youtube);
+		this.setTwitter(twitter);
+		this.setFacebook(facebook);
+		this.setLinkedin(linkedin);
+		this.setInstagram(instagram);
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
 		this.id = id;
+	}
+	public int getProfile_id() {
+		return profile_id;
+	}
+	public void setProfile_id(int profile_id) {
 		this.profile_id = profile_id;
+	}
+	public String getYoutube() {
+		return youtube;
+	}
+	public void setYoutube(String youtube) {
 		this.youtube = youtube;
+	}
+	public String getTwitter() {
+		return twitter;
+	}
+	public void setTwitter(String twitter) {
 		this.twitter = twitter;
-				
+	}
+	public String getFacebook() {
+		return facebook;
+	}
+	public void setFacebook(String facebook) {
+		this.facebook = facebook;
+	}
+	public String getLinkedin() {
+		return linkedin;
+	}
+	public void setLinkedin(String linkedin) {
+		this.linkedin = linkedin;
+	}
+	public String getInstagram() {
+		return instagram;
+	}
+	public void setInstagram(String instagram) {
+		this.instagram = instagram;
 	}
 }
